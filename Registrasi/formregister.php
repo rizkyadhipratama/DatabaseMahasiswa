7c5fb20e186f9c9b0ff1678b589024de7c4cce9c
rizkyadhipratama <67497659+rizkyadhipratama@users.noreply.github.com>
2020-10-04 20:49:40 +0700

    Create formregister.php

---
+++ Registrasi/formregister.php
@@ -0,0 +1,21 @@
+<?php
+
+//include "index.php";
+//membuat form
+echo"<h2>Input Data Mahasiswa</h2>";
+echo "<form method=post action=prosesregistrasi.php><table border=0>";
+echo "<tr><td>Nama </td><td>: <input type=text name=nama> </td></tr>";
+echo "<tr><td>Username</td><td>: <input type=text name=username> </td></tr>";
+echo "<tr><td>Password</td><td>: <input type=text name=password> </td></tr>";
+echo "<tr><td>Level</td><td>: <input type=text name=level> </td></tr>";
+
+
+echo "<tr><td><input type=submit value=Input></td></tr>"; //tombol input
+
+echo "</table></form>";
+	
+	/*
+	Febrio Rizky A.
+	21060118120033
+	*/
+?>
