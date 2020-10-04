d61d0f69fa5e984902931805ea87520f68240e4d
rizkyadhipratama <67497659+rizkyadhipratama@users.noreply.github.com>
2020-10-04 20:51:39 +0700

    Create formeditakun.php

---
+++ UpdatePassword/formeditakun.php
@@ -0,0 +1,23 @@
+<?php
+
+//include "index.php";
+//membuat form
+echo"<h2>Edit Akun Mahasiswa</h2>";
+echo "<form method=post action=editakun3.php><table border=0>";
+// echo "<tr><td>Username Lama</td><td>: <input type=text name=unamelama> </td></tr>";
+// echo "<tr><td>Username Baru</td><td>: <input type=text name=unamebaru> </td></tr>";
+echo "<tr><td>Password Lama</td><td>: <input type=password name=passwordlama> </td></tr>";
+echo "<tr><td>Password Baru</td><td>: <input type=password name=passwordbaru> </td></tr>";
+echo "<tr><td>Konfirmasi Password</td><td>: <input type=password name=konfirmpassword> </td></tr>";
+echo "<tr><td>Level</td><td>: <input type=text name=level> </td></tr>";
+
+
+echo "<tr><td><input type=submit value=Input></td></tr>"; //tombol input
+
+echo "</table></form>";
+	
+	/*
+	Febrio Rizky A.
+	21060118120033
+	*/
+?>
