30844681bf3bac492a83ce5be483dca519c07f48
rizkyadhipratama <67497659+rizkyadhipratama@users.noreply.github.com>
2020-10-04 20:31:06 +0700

    Create menu.php
    Berisi menu navigasi halaman

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+echo"<h2 font color=green>
+<a href=index.php>Home</a> ||
+<a href=formmhs.php>Input</a> || 
+<a href=tampil.php>Tampil</a> || 
+<a href=formcari.php>Cari</a> || 
+<a href=formeditakun.php>Edit Akun</a> ||
+<a href=logout.php>Logout</a> <br></h2>";
+?>
