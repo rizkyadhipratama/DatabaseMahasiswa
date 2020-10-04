d858038c7aa54022e091f9372802c73e4ce8b62a
rizkyadhipratama <67497659+rizkyadhipratama@users.noreply.github.com>
2020-10-04 20:25:41 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<h2>Selamat Datang</h2>
+<?php
+echo"<table border=0>";
+echo"<h2>login</h2>";
+echo "<form method=post action=index.php><table border=0>";
+echo "<tr><td>username </td><td>: <input type=text name=username> </td></tr>";
+echo "<tr><td>password </td><td>: <input type=password name=password> </td></tr>";
+
+echo "<tr><td><input type=submit value=Input></td></tr>"; 
+echo "<tr><td><a href=formregister.php>Register</a></td></tr>";
+
+echo "</table></form>";
+
+	/*
+	Febrio Rizky A.
+	21060118120033
+	*/
+?>
